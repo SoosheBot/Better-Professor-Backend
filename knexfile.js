3252687429558b4382771ab6494bf7dcf8f4add9
sooshe <sooshe@sooshebot.com>
2020-03-01 19:48:56 -0600

    add and edit knexfile

---
+++ knexfile.js
@@ -0,0 +1,43 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/betterProfDB',
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+  testing: {
+    client: 'pg',
+    connection: "postgresql://localhost/testing",
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations",
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  }
+
+};

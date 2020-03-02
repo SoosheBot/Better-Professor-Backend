15ed2a5cfdf5b7c9268e5292d98affbd9e75d42c
sooshe <sooshe@sooshebot.com>
2020-03-01 19:40:15 -0600

    add tests and logger middleware

--- api/server.js
+++ api/server.js
@@ -2,16 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const logger = require('../middleware/logger');
+
 // const authenticate = require("../auth/auth-middleware.js");
 // const authRouter = require("../auth/auth-router.js");
 // const usersRouter = require("../users/users-router.js");
 // const studentsRouter = require("../students/students-router");
+
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(logger);
 
 // server.use("/api/auth", authRouter);
 // server.use("/api/users", authenticate, usersRouter);

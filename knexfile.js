87fdc09682f80619b02d1fbc201f0c878d4e5f2b
sooshe <sooshe@sooshebot.com>
2020-03-03 17:53:52 -0600

    delete database

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/betterProfDB',
+    connection: 'postgres://localhost/betterProf',
     migrations: {
       directory: './data/migrations',
     },

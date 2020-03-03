7eee95d7fe87aab2226ff93024dc5bd34dfd4401
sooshe <sooshe@sooshebot.com>
2020-03-03 16:19:49 -0600

    update deadlines-model

--- deadlines/deadlines-model.js
+++ deadlines/deadlines-model.js
@@ -19,3 +19,33 @@ function find() {
       .insert(deadline, "id")
       .then(([id]) => find(id));
   }
+
+  function findBy(filter) {
+    // return db("tasks as t")
+    //   .select("t.name as name", "d.due_date as due_date" )
+    //   .join("deadlines as d", "t.deadline_id", "=", "d.id")
+    //   .where(filter);
+    return db("deadlines")
+      .select("*")
+      .where(filter);
+  }
+
+  function findById(id) {
+    return db("deadlines")
+      .where({ id })
+      .first();
+  }
+
+  function update(changes, id) {
+    return db("deadlines", "id")
+      .where({ id })
+      .update(changes, "*")
+      .then(count => findById(id));
+  }
+
+  function remove(id) {
+    return db("deadlines")
+      .where({ id })
+      .del();
+  }
+

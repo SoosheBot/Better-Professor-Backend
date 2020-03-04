d75c947dffe5e45682bd9e43de74b8f7e73103a1
sooshe <sooshe@sooshebot.com>
2020-03-03 18:53:52 -0600

    update users

--- database/migrations/20200303181254_info.js
+++ database/migrations/20200303181254_info.js
@@ -1,27 +1,27 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("info", function(info) {
+    return knex.schema.createTable("info", function(info) {
+        info
+        .integer("user_id")
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");  
+        info
+        .integer("task_id")
+        .references("id")
+        .inTable("tasks")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");  
+  
     info
-      .integer("user_id")
-      .references("id")
-      .inTable("users")
-      .onDelete("CASCADE")
-      .onUpdate("CASCADE");
-    info
-      .integer("task_id")
-      .references("id")
-      .inTable("tasks")
-      .onDelete("CASCADE")
-      .onUpdate("CASCADE");
-    info
-      .integer("deadline_id")
-      .references("id")
-      .inTable("deadlines")
-      .onDelete("CASCADE")
-      .onUpdate("CASCADE");
-  });
+        .integer("deadline_id")
+        .references("id")
+        .inTable("deadlines")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");  
+    })
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists("info");
-  };
+exports.down = function(knex) {
   
+};

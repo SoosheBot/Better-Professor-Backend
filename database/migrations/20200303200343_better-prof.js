d0770284232f1cf416df12cdfe64963c657b298b
sooshe <sooshe@sooshebot.com>
2020-03-03 20:12:48 -0600

    update migrations

---
+++ database/migrations/20200303200343_better-prof.js
@@ -0,0 +1,73 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable("deadlines", tbl => {
+      tbl.increments();
+      tbl.date("due_date").notNullable();
+    })
+
+    .createTable("tasks", tbl => {
+      tbl.increments();
+      tbl.text("name", 128).notNullable();
+      tbl
+        .integer("deadline_id")
+        .references("id")
+        .inTable("deadlines")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+
+    .createTable("users", tbl => {
+      tbl.increments();
+      tbl.string("lastname", 128).notNullable();
+      tbl.string("firstname", 128).notNullable();
+      tbl.string("username", 128).notNullable();
+      tbl.string("password", 128).notNullable();
+      tbl
+        .string("email", 128)
+        .notNullable()
+        .unique();
+      tbl.string("role").defaultTo("user");
+      tbl
+        .integer("task_id")
+        .references("id")
+        .inTable("tasks")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("deadline_id")
+        .references("id")
+        .inTable("deadlines")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+
+    .createTable("info", tbl => {
+      tbl.increments();
+      tbl
+        .integer("deadline_id")
+        .references("id")
+        .inTable("deadlines")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("task_id")
+        .references("id")
+        .inTable("tasks")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("user_id")
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTableIfExists("info")
+    .dropTableIfExists("users")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("deadlines");
+};

957779984af01fad14ae050c8589aed61c8de475
sooshe <sooshe@sooshebot.com>
2020-03-05 21:09:14 -0600

    remove authenticate from server router -students

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ server.use(logger);
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", authenticate, usersRouter);
-server.use("/api/students", authenticate, studentsRouter);
+server.use("/api/students", studentsRouter);
 server.use("/api/tasks", tasksRouter);
 server.use("/api/messages", messagesRouter);
 
